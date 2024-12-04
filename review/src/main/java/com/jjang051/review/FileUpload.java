67444e3c55531632d09bc0ef7ad66f0a6a785bbe
jjang051tj <jjang051tj@gmail.com>
2024-12-04 16:06:45 +0900

    유 db connection

--- review/src/main/java/com/jjang051/review/FileUpload.java
+++ review/src/main/java/com/jjang051/review/FileUpload.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Part;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @WebServlet("/file-upload")
 @MultipartConfig
@@ -24,13 +25,20 @@ public class FileUpload  extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String title = req.getParameter("title");
         Part filePart = req.getPart("attachFile");
-        String partHeader = filePart.getHeader("content-disposition");
-
+        //String partHeader = filePart.getHeader("content-disposition");
         //String attachFile = req.getParameter("attachFile"); //이건 스트링 처리용 file은 못받음
-        System.out.println("title==="+title);
-        System.out.println("partHeader==="+partHeader);
-        String[] partHeaderArray = partHeader.split("filename=");
-        String originalFileName = partHeaderArray[1].replace("\"","");
+        //System.out.println("title==="+title);
+        //System.out.println("partHeader==="+partHeader);
+        //String[] partHeaderArray = partHeader.split("filename=");
+        //String originalFileName = partHeaderArray[1].replace("\"","");
+        String originalFileName = filePart.getSubmittedFileName();
         System.out.println("originalFileName==="+originalFileName);
+        String extention = originalFileName.substring(originalFileName.lastIndexOf(".")+1);
+        String extractFileName = originalFileName.substring(0,originalFileName.lastIndexOf("."));
+        UUID uuid = UUID.randomUUID();
+        String uuidStr = uuid.toString();
+        String renameFileName = extractFileName+"-"+uuidStr+"."+extention;
+        System.out.println("originalFileName==="+originalFileName);
+        System.out.println("renameFileName==="+renameFileName);
     }
 }
